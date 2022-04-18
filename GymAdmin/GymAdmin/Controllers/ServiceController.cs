c804d7e3f599a24494673602d7670259069290b0
dagemov <sebcoss1234@gmail.com>
2022-04-17 22:26:27 -0500

    cambiar el amarre de Servicies profesinonal

---
+++ GymAdmin/GymAdmin/Controllers/ServiceController.cs
@@ -0,0 +1,303 @@
+﻿using GymAdmin.Common;
+using GymAdmin.Data;
+using GymAdmin.Data.Entities;
+using GymAdmin.Enums;
+using GymAdmin.Helpers;
+using GymAdmin.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymAdmin.Controllers
+{
+
+    public class ServiceController : Controller
+    {
+        private readonly DataContext _context;
+        private readonly IUserHelper _userHelper;
+        private readonly IBlobHelper _blobHelper;
+        private readonly IMailHelper _mailHelper;
+
+        public ServiceController(DataContext context, IUserHelper userHelper, IBlobHelper blobHelper, IMailHelper mailHelper)
+        {
+            _context = context;
+            _userHelper = userHelper;
+            _blobHelper = blobHelper;
+            _mailHelper = mailHelper;
+        }
+
+        // GET: Services
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Services.ToListAsync());
+        }
+
+        // GET: Services/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var service = await _context.Services
+                .Include(s => s.Professionals)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (service == null)
+            {
+                return NotFound();
+            }
+
+            return View(service);
+        }
+
+        // GET: Services/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Services/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Service service)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(service);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(service);
+        }
+
+        // GET: Services/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var service = await _context.Services.FindAsync(id);
+            if (service == null)
+            {
+                return NotFound();
+            }
+            return View(service);
+        }
+
+        // POST: Services/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Price")] Service service)
+        {
+            if (id != service.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(service);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ServiceExists(service.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(service);
+        }
+
+        // GET: Services/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var service = await _context.Services
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (service == null)
+            {
+                return NotFound();
+            }
+
+            return View(service);
+        }
+
+        // POST: Services/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var service = await _context.Services.FindAsync(id);
+            _context.Services.Remove(service);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ServiceExists(int id)
+        {
+            return _context.Services.Any(e => e.Id == id);
+        }
+
+        //Get
+        /*
+        public async Task<IActionResult> AddProfessional(int? id)
+        {
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Service service = await _context.Services.FindAsync(id);
+
+            if (service == null)
+            {
+                return NotFound();
+            }
+
+            AddUserProfessionalViewModel model = new()
+            {
+                ServiceId =service.Id
+            };
+            return View(model);
+        }
+
+        [HttpPost, ActionName("Register")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddProfessional(AddUserProfessionalViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    UserProfessional userPro = new()
+                    {
+                        service = await _context.Services.FindAsync(model.ServiceId),
+                        UserName= model.Username,
+                        Password = model.Password,
+                        c = model.ImageId,
+                        UserType= model.UserType,
+                        
+                        //there's left the list of the day that the userprofessional give his asesories
+
+                    };
+                    _context.Add(userPro);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Details), new { Id = model.ServiceId });
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "Ya existe este profesional en este servicio.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+            }
+
+            return View(model);
+        }
+        */
+        public IActionResult CreateProfessional()
+        {
+            AddUserViewModel model = new()
+            {
+                Id = Guid.Empty.ToString(),
+                UserType = UserType.Professional
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateProfessional(AddUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Guid imageId = Guid.Empty;
+
+                if (model.ImageFile != null)
+                {
+                    imageId = await _blobHelper.UploadBlobAsync(model.ImageFile, "users");
+                }
+
+                User user = await _userHelper.AddUserAsync(model, imageId);
+                if (user == null)
+                {
+                    ModelState.AddModelError(string.Empty, "¡Este correo ya está en uso!");
+                    return View(model);
+                }
+
+                //Email confirmation
+                string token = await _userHelper.GenerateEmailConfirmationTokenAsync(user);
+                string tokenLink = Url.Action(
+                    "ConfirmEmail",
+                    "Account",
+                    new
+                    {
+                        UserId = user.Id,
+                        Token = token
+                    },
+                    protocol: HttpContext.Request.Scheme);
+
+                string body = "<style>body{text-align:center;font-family:Verdana,Arial;}</style>" +
+                    $"<h1>Soporte GymAdmin</h1>" +
+                    $"<h3>Estás a un solo paso de ser parte de nuestra comunidad de Profesionales</h3>" +
+                    $"<h4>Sólo debes hacer click en el siguiente botón para confirmar tu email</h4>" +
+                    $"<br/>" +
+                    $"<a style=\"padding:15px;background-color:#f1b00e;text-decoration:none;color:black;border: 5px solid #000;border-radius:10px;\" href=\"{tokenLink}\">Confirmar email</a>";
+
+                Response response = _mailHelper.SendMail(
+                    user.FullName,
+                    model.Username,
+                    "GymAdmin - Confirmación del email",
+                    body);
+
+                if (response.IsSuccess)
+                {
+                    return RedirectToAction("ConfirmEmailMessage", "Account");
+                }
+                else
+                {
+                    return RedirectToAction("ConfirmEmailErrorMessage", "Account");
+                }
+            }
+            return View(model);
+        }
+
+        /*
+         * SCHEDULE ACTIONS
+         */
+
+    }
+
+    
+}
---
+++ GymAdmin/GymAdmin/Migrations/20220418021955_initialUsers.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GymAdmin.Migrations
+{
+    public partial class initialUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
