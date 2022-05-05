9e968a9eb2f2a5900448d5e0dc21be5abb8233ca
Dagemov <sebcoss1234@gmail.com>
2022-05-05 14:07:13 -0500

    terminar curd Director (shecudles) debatir

--- GymAdmin/GymAdmin/Controllers/EventController.cs
+++ GymAdmin/GymAdmin/Controllers/EventController.cs
@@ -185,6 +185,147 @@ namespace GymAdmin.Controllers
             return View(model);
         }
 
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Event evento = await _context.Events
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (evento == null)
+            {
+                return NotFound();
+            }
+
+            return View(evento);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            Event evento = await _context.Events.FindAsync(id);
+            _context.Events.Remove(evento);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        //--------------                    Director                -------------------
+
+        public async Task<IActionResult> DetailsDirector(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Director director   = await _context.Directors
+                .Include(p => p.User)
+                .Include(p => p.Schedule)
+                .Include(p => p.Events)
+                .FirstOrDefaultAsync(p => p.Id == id);
+
+            return View(director);
+        }
+        public async Task<IActionResult> CreateProfessional()
+        {
+            AddProfessionalViewModel model = new()
+            {
+                Id = Guid.Empty.ToString(),
+                UserType = UserType.User,
+                Schedules = await _combosHelper.GetComboSchedulesAsync()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateDirector(AddDirectorViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.ScheduleId == 0)
+                {
+                    model.Schedules = await _combosHelper.GetComboSchedulesAsync();
+                    ModelState.AddModelError(string.Empty, "¡Debe seleccionar un horario!");
+                    return View(model);
+                }
+
+                User userDocumentExist = await _userHelper.GetUserAsync(model);
+                if (userDocumentExist != null)
+                {
+                    model.Schedules = await _combosHelper.GetComboSchedulesAsync();
+                    ModelState.AddModelError(string.Empty, "¡Ya existe un usuario con este documento!");
+                    return View(model);
+                }
+
+                Guid imageId = Guid.Empty;
+                if (model.ImageFile != null)
+                {
+                    imageId = await _blobHelper.UploadBlobAsync(model.ImageFile, "users");
+                }
+
+                User user = await _userHelper.AddUserAsync(model, imageId);
+                if (user == null)
+                {
+                    await _blobHelper.DeleteBlobAsync(imageId, "users");
+                    model.Schedules = await _combosHelper.GetComboSchedulesAsync();
+                    ModelState.AddModelError(string.Empty, "¡Este correo ya está en uso!");
+                    return View(model);
+                }
+
+                //Relation user - professional
+                Professional professional = new()
+                {
+                    User = user,
+                    ProfessionalType = model.ProfessionalType,
+                    Schedule = await _context.Schedules.FindAsync(model.ScheduleId)
+                };
+                _context.Add(professional);
+                await _context.SaveChangesAsync();
+
+                //Email confirmation
+                string token = await _userHelper.GenerateEmailConfirmationTokenAsync(user);
+                string tokenLink = Url.Action(
+                    "ConfirmEmail",
+                    "Account",
+                    new
+                    {
+                        UserId = user.Id,
+                        Token = token
+                    },
+                    protocol: HttpContext.Request.Scheme);
+
+                string body = "<style>body{text-align:center;font-family:Verdana,Arial;}</style>" +
+                    $"<h1>Soporte GymAdmin</h1>" +
+                    $"<h3>Estás a un solo paso de ser parte de nuestra comunidad</h3>" +
+                    $"<h4>Sólo debes hacer click en el siguiente botón para confirmar tu email</h4>" +
+                    $"<br/>" +
+                    $"<a style=\"padding:15px;background-color:#f1b00e;text-decoration:none;color:black;border: 5px solid #000;border-radius:10px;\" href=\"{tokenLink}\">Confirmar email</a>";
+
+                Response response = _mailHelper.SendMail(
+                    user.FullName,
+                    model.Username,
+                    "GymAdmin - Confirmación del email",
+                    body);
+
+                if (response.IsSuccess)
+                {
+                    return RedirectToAction("ConfirmEmailMessage", "Account");
+                }
+                else
+                {
+                    return RedirectToAction("ConfirmEmailErrorMessage", "Account");
+                }
+            }
+
+            model.Schedules = await _combosHelper.GetComboSchedulesAsync();
+            return View(model);
+        }
 
     }
 }
